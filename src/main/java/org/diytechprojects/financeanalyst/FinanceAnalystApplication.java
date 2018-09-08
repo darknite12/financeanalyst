a9d1889712171f221dcb3314ce71e5d81914718d
Franisco <flara.commercial@gmail.com>
2018-09-08 15:54:44 -0400

    #4 Setup Spring Boot Project

---
+++ src/main/java/org/diytechprojects/financeanalyst/FinanceAnalystApplication.java
@@ -0,0 +1,13 @@
+package org.diytechprojects.financeanalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAnalystApplication {
+	
+	public static void main(String[] args) {
+	    SpringApplication.run(FinanceAnalystApplication.class, args);
+	  }
+
+}
